25927177e7b5590c8ba23eec636628e28295798b
aagamaar <aagamaar01@gmail.com>
2025-10-09 19:49:37 +0530

    changed variables to avoid confusion

--- p19.R
+++ p19.R
@@ -1,10 +1,10 @@
-states <- data.frame(
+data <- data.frame(
 State = c("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware"),
 Population = c(4779736, 719231, 6392017, 2915918, 37253956, 5029196, 3574097, 897934),
 Murder = c(5.7, 5.6, 4.7, 5.6, 4.4, 2.8, 2.4, 5.8)
 )
-population <- states$Population
-mean <- mean(population)
-median <- median(population)
-variance <- var(population)
+pop <- data$Population
+mean <- mean(pop)
+median <- median(pop)
+variance <- var(pop)
 cat("Mean:", mean, "\nMedian:", median, "\nVariance:", variance)
