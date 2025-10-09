194b5c655953204b46dd9018df0c7fc6f2abb2b9
aagamaar <aagamaar01@gmail.com>
2025-10-09 22:45:49 +0530

    changed the code

--- p16.R
+++ p16.R
@@ -1,3 +1,3 @@
-ages <- c("17"=5, "18"=40, "19"=30,"20"=20,"21"=5)
+grades <- c(A=20, B=40, C=30, D=10, E=25)
 colors <- c("#1f77b4", "#1fe23f", "#2de3ff","#feddf0", "#fff000")
-pie(ages, labels = paste(names(ages), "(", ages, "%)", sep = ""), col = colors, main = "Ages of Freshers")
+pie(grades, labels = paste(names(grades), "(", grades, "%)", sep = ""), col = colors, main = "Percentage of grades in class")
