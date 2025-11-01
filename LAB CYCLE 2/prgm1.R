7e0eee1db083c90788f4738e063ab1369f1356e0
aagamaar <aagamaar01@gmail.com>
2025-11-01 20:02:42 +0530

    Added program about basic input output and operation variables

---
+++ LAB CYCLE 2/prgm1.R
@@ -0,0 +1,26 @@
+num_var <- 10
+int_var <- 20L
+char_var <- " Hello World ! "
+logical_var <- TRUE
+complex_var <- 4+6i
+numeric_vector <- c(1,2,3,4,5)
+character_vector <- c("apple", "banana", "strawberry")
+logical_vector <- c(TRUE, FALSE, TRUE, FALSE)
+matrix_data <- matrix(1:9, nrow=3, ncol=3, byrow= TRUE)
+list_data <- list(name="Arun", age=30, city="NewYork")
+data_frame <- data.frame(
+  name=c("Aagama", "Arun", "Amrita", "Ananya"),
+  age=c(20,22,17,12),
+  city=c("Dubai", "N.Paravur", "Dubai", "N.Paravur")
+)
+print(num_var)
+print(int_var)
+print(char_var)
+print(logical_var)
+print(complex_var)
+print(numeric_vector)
+print(character_vector)
+print(logical_vector)
+print(matrix_data)
+print(list_data)
+print(data_frame)
